f1b1e0bbc20311eb7351168f1e3b539e5f2215ac
samishken <samishken@gmail.com>
2019-10-23 13:31:37 -0400

    printfun training

---
+++ printfun.py
@@ -0,0 +1,16 @@
+print()
+print("Hello"*3)
+print("All the power \n is within you")
+
+a,b=10,20
+print(a,b)
+print(a,b,sep=',')
+print(a,b,sep='++++')
+
+name="jon"
+marks=94.5
+
+print("Name is ", name, "marks are", marks)
+print("Name is %s, Marks are %.2f"%(name,marks))
+
+print("Name is {}, Marks are {}".format(name,marks))
